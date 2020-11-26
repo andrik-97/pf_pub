82985b907187e6dc1bdc05ae79fb1d428902afe3
Andri Kusuma <andri.kusuma@payfazz.com>
2020-11-26 12:05:43 +0700

    add publish module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/andrik-97/pf_pub
 
 go 1.15
+
+require cloud.google.com/go/pubsub v1.8.3 // indirect
---
+++ pf_pub.go
@@ -0,0 +1,51 @@
+package pf_pub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+)
+
+const (
+	projectID string = "d291209"
+	topicID   string = "pubsub_test"
+)
+
+// User for contract on schema user
+type User struct {
+	UserID    int       `json:"id"`
+	Name      string    `json:"name"`
+	Gender    string    `json:"gender"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// PublishUser for publishing user to pubsub
+func PublishUser(u User) error {
+	message, _ := json.Marshal(u)
+	err := publish(projectID, topicID, string(message))
+	return err
+}
+
+func publish(projectID, topicID, msg string) error {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+
+	t := client.Topic(topicID)
+	result := t.Publish(ctx, &pubsub.Message{
+		Data: []byte(msg),
+	})
+	// Block until the result is returned and a server-generated
+	// ID is returned for the published message.
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("Get: %v", err)
+	}
+	fmt.Printf("Published a message; msg ID: %v\n", id)
+	return nil
+}
